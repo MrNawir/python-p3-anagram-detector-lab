2a11af9c0d31e0e98d82a303a290431e9b05d8d2
Ibrahim Abdu <ibrahim.abdu@student.moringaschool.com>
2025-11-18 14:02:10 +0300

    Completed lab

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,14 @@
 # your code goes here!
+
+class Anagram:
+
+    def __init__(self, word):
+        self.word = word
+
+    def match(self, candidates):
+        matches = []
+        target = sorted(self.word.lower())
+        for candidate in candidates:
+            if sorted(candidate.lower()) == target and candidate.lower() != self.word.lower():
+                matches.append(candidate)
+        return matches
